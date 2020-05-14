f69740a5513b4cb6778cedad7940b69b4248b35e
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-05-13 22:22:23 -0500

    Este es el commit hecho durante el video 028 Solucion Anadir enlace a los proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class Project(models.Model):
     title   = models.CharField(max_length=200, verbose_name = "Título")
     ddescription = models.TextField( verbose_name = "Descripción")
+    link = models.URLField(verbose_name = "Dirección web",null=True, blank=True)
     image   = models.ImageField( verbose_name = "Imagen", upload_to="projets")
     created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creación")
     updated = models.DateTimeField(auto_now=True,  verbose_name = "Fecha de edición")
-
+    
     class Meta:
         verbose_name = "proyecto"
         verbose_name_plural = "proyectos"
